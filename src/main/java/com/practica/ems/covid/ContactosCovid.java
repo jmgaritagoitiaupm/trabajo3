1ba37fe6bfce945dd2b954816f08fdb3a6825cec
jaimesanjosevillar <jaime.sanjose.villar@alumnos.upm.es>
2022-05-07 17:03:35 +0200

    Reducción de loadDataFile issue #9

--- src/main/java/com/practica/ems/covid/ContactosCovid.java
+++ src/main/java/com/practica/ems/covid/ContactosCovid.java
@@ -69,36 +69,22 @@ public class ContactosCovid {
 	}
 
 	public void loadDataFile(String fichero, boolean reset) {
-		File archivo = null;
-		FileReader fr = null;
-		BufferedReader br = null;
-		String datas[] = null, data = null;
-		loadDataFile(fichero, reset, archivo, fr, br, datas, data);
-		
-	}
-
-	@SuppressWarnings("resource")
-	public void loadDataFile(String fichero, boolean reset, File archivo, FileReader fr, BufferedReader br, String datas[], String data ) {
 		try {
-			archivo = new File(fichero);
-			fr = new FileReader(archivo);
-			br = new BufferedReader(fr);
+			File archivo = new File(fichero);
+			FileReader fr = new FileReader(archivo);
+			BufferedReader br = new BufferedReader(fr);
+			String datas[] = null, data = null;
 			if (reset) {this.reseter();}
 			while ((data = br.readLine()) != null) {
 				this.dataLoader(datas, data);
 			}
+			if (null != fr) {
+				fr.close();
+			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				if (null != fr) {
-					fr.close();
-				}
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
-		}
+		} 
 	}
 	
 	public void dataLoader(String datas[], String data) throws EmsInvalidTypeException, EmsInvalidNumberOfDataException,
